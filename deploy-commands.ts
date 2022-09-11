68a2b82330f9c82387a287e8b7d4d45dae319de1
dindin-nibnib <mail@dindin.ch>
2022-09-11 22:14:39 +0200

    added shebang to deploying script

--- deploy-commands.ts
+++ deploy-commands.ts
@@ -1,3 +1,4 @@
+#!/root/.nvm/versions/node/v16.17.0/bin/ts-node
 /* It's importing the required modules. */
 import fs = require("fs");
 import path = require("path");
@@ -16,21 +17,21 @@ const token = process.env.TOKEN;
 /* It's checking if the `clientId` is defined in the `.env` file. If it is, it will set the `clientId`
 variable to the value of the `clientId` in the `.env` file. */
 if (process.env.clientId !== undefined) {
-  clientId = process.env.clientId;
+	clientId = process.env.clientId;
 }
 
 /* It's checking if the `guildId` is defined in the `.env` file. If it is, it will set the `guildId`
 variable to the value of the `guildId` in the `.env` file. */
 if (process.env.guildId !== undefined) {
-  clientId = process.env.guildId;
+	clientId = process.env.guildId;
 }
 
 console.log("Imported dotenv");
 
 /* It's checking if the `token` is defined in the `.env` file. If it isn't, it will throw an error. */
 if (token === undefined) {
-  console.error("Invalid token: " + token);
-  process.exit(-1);
+	console.error("Invalid token: " + token);
+	process.exit(-1);
 }
 
 console.log("Valid token");
@@ -39,21 +40,21 @@ console.log("Valid token");
 const commands = [];
 const commandsPath = path.join(__dirname, "src/commands");
 const commandFiles = fs
-  .readdirSync(commandsPath)
-  .filter((file) => file.endsWith(".ts"));
+	.readdirSync(commandsPath)
+	.filter((file) => file.endsWith(".ts"));
 
 /* It's looping through all the files in the `commands` folder and adding them to the `commands` array. */
 for (const file of commandFiles) {
-  const filePath = path.join(commandsPath, file);
-  const command = require(filePath);
-  commands.push(command.data.toJSON());
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	commands.push(command.data.toJSON());
 }
 
 commands.push(
-  new SlashCommandBuilder()
-    .setName("reload")
-    .setDescription("reloads the commands")
-    .toJSON()
+	new SlashCommandBuilder()
+		.setName("reload")
+		.setDescription("reloads the commands")
+		.toJSON()
 );
 
 /* It's creating a new REST instance with the version of the Discord API being used. */
@@ -61,6 +62,6 @@ const rest = new REST({ version: "9" }).setToken(token);
 
 /* It's registering the commands. */
 rest
-  .put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-  .then(() => console.log("Successfully registered application commands."))
-  .catch(console.error);
+	.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+	.then(() => console.log("Successfully registered application commands."))
+	.catch(console.error);
