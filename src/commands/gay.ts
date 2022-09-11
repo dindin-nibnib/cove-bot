ef6bf6f145d31227a0b01ef4f45d07753eeb5393
dindin-nibnib <mail@dindin.ch>
2022-09-11 22:30:41 +0200

    added le gay rating

---
+++ src/commands/gay.ts
@@ -0,0 +1,37 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import Discord = require("discord.js");
+import Discordx = require("discordx");
+import { randInt } from "../lib/utilities";
+
+// Redeclares Client in order to add a collection of commands
+class Client extends Discordx.Client {
+	commands = new Discord.Collection();
+}
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("gayrate")
+		.setDescription("Rates you on how much of a gay pal you are."),
+
+	async execute(
+		interaction: Discord.CommandInteraction<Discord.CacheType>,
+		client: Client
+	) {
+		let rating = 0;
+		if (interaction.user.id === "592865976501141569")
+			rating = randInt(98, 100);
+		else if (interaction.user.id === "373515998000840714")
+			rating = randInt(95, 100);
+		else
+			rating = randInt();
+
+		await interaction.reply({
+			embeds: [
+				new Discord.MessageEmbed()
+					.setTitle("Gay rating")
+					.setDescription(`You're ${rating}% gay!`)
+					.setColor("BLURPLE")
+			]
+		});
+	},
+};
