c48b3ccc35ef8b83a2fddfa484ba54ac1160b391
dindin-nibnib <mail@dindin.ch>
2022-09-12 19:41:27 +0200

    added comment

--- src/lib/utilities.ts
+++ src/lib/utilities.ts
@@ -1,3 +1,7 @@
+/*
+ * randInt
+ * Returns a random integer between min (inclusive) and max (exclusive)
+*/
 export const randInt = (min: number = 0, max: number = 100) => {
 	return Math.floor(Math.random() * (max - min + 1) + min);
 };
